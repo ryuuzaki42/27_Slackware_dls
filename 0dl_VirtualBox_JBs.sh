4f7a8c8d33c2c68ee7335fca7c7f3f353e1336f1
João D. Batista <joao42lbatista@gmail.com>
2021-11-23 04:18:59 -0300

    0dl_VirtualBox_JBs.sh - up "6.1.28" to "6.1.30"

--- 0dl_VirtualBox_JBs.sh
+++ 0dl_VirtualBox_JBs.sh
@@ -21,7 +21,7 @@
 #
 # Descrição: Script to download the last version VirtualBox
 #
-# Last update: 24/08/2021
+# Last update: 23/11/2021
 #
 case "$(uname -m)" in
     i?86) archDL="x86" ;;
@@ -29,7 +29,7 @@ case "$(uname -m)" in
     *) archDL=$(uname -m) ;;
 esac
 
-progName="virtualbox" # last tested: "6.1.28"
+progName="virtualbox" # last tested: "6.1.30"
 
 linkGetVersion="https://www.virtualbox.org/wiki/Downloads"
 wget "$linkGetVersion" -O "${progName}_latest"
